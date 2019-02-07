bfce65f4b4d674b5bae38cd8d720b0d79ac54890
Claista Guibert <gclaista@gmail.com>
2019-02-07 22:02:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase())
   return ('YES INDEED!');
 }
+function sayHiToGrandma(string) {
+  else (string === ('I love you, Grandma.'))
+  return ('I love you, too.');
+}
